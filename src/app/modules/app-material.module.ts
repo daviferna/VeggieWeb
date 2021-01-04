5a4c256d32d109c3b19de653b0f0470f39e203ea
vvaquerizo <vvaquerizo@zennio.com>
2021-01-04 22:48:42 +0100

    Sustituidos componentes estándar por Material en página de Contacto

--- src/app/modules/app-material.module.ts
+++ src/app/modules/app-material.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 
 @NgModule({
     imports: [
         MatToolbarModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatInputModule
     ],
     exports: [
         MatToolbarModule,
         MatIconModule,
-        MatButtonModule
+        MatButtonModule,
+        MatInputModule
     ],
     providers:[]
   })
