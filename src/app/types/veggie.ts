54e4ec3adecd9ec8b51175415d7ac9204e6495ba
daniel.avila <daniel.avilaf@outlook.es>
2021-01-07 16:52:39 +0100

    Añadidos estilos y materialización para la vista del listado. Añadidos más elementos a los datos "fake" para comprobar columnas y filas.

--- src/app/types/veggie.ts
+++ src/app/types/veggie.ts
@@ -1,12 +1,14 @@
 export class Veggie{
     id: number;
     name:string;
+    type:string;
     description:string;
     photoUrl:string;
 
-    constructor(id:number, name:string, description:string, photoUrl:string){
+    constructor(id:number, name:string, type:string, description:string, photoUrl:string){
         this.id = id;
         this.name = name;
+        this.type = type;
         this.description = description;
         this.photoUrl = photoUrl;
     }
