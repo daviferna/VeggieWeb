16447c7787fc16e6e0e112f9f50765ab2ad1a326
vvaquerizo <vvaquerizo@zennio.com>
2021-01-08 23:30:43 +0100

    Diversas correcciones, mejoras accesibilidad y traducciones en Contacto

--- src/app/components/contact-page.component/contact-page.component.ts
+++ src/app/components/contact-page.component/contact-page.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators, ReactiveFormsModule, FormGroup} from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core'
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  commentForm!: FormGroup;
+  @ViewChild('formDirective') formDirective!: NgForm;
+
+  translate: TranslateService;
+  titleService: Title;
+
+  constructor( translate: TranslateService, titleService: Title ) {
+    this.translate = translate;
+    this.titleService = titleService;
+  }
 
   ngOnInit(): void {
+    this.translate.get('contactPage.tabTitle').subscribe((translated: string) => {
+      this.titleService.setTitle(translated);
+    }),
+    this.commentForm = new FormGroup({
+      name: this.nameFormControl,
+      email: this.emailFormControl,
+      type: this.typeFormControl,
+      comment: this.commentFormControl
+    });
+  }
+
+  nameFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  typeFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  commentFormControl = new FormControl('', [
+    Validators.required,
+  ]);
+
+  submitForm() {
+    if (this.commentForm.valid) {
+      alert(this.translate.instant('contactPage.submitFormValid'));
+      this.formDirective.resetForm();
+    } else {
+      alert(this.translate.instant('contactPage.submitFormInvalid'));
+    }
   }
 
 }
--- src/app/modules/app-material.module.ts
+++ src/app/modules/app-material.module.ts
@@ -3,20 +3,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
-
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
-        MatInputModule
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule
     ],
     exports: [
         MatToolbarModule,
         MatIconModule,
         MatButtonModule,
-        MatInputModule
+        MatInputModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule
     ],
     providers:[]
   })
