f05aea5444ec5e63004cff53fc6175d938d16a18
daniel.avila <daniel.avilaf@outlook.es>
2021-01-08 03:09:38 +0100

    Añadidos estilos a la página del detalle de veggies y añadido contenido a la página de home

--- src/app/types/plantInstruction.ts
+++ src/app/types/plantInstruction.ts
@@ -1,8 +1,10 @@
 export class PlantInstruction{
+    title:string;
     description:string;
     photoUrl:string;
 
-    constructor(description:string, photoUrl:string) {
+    constructor(title:string, description:string, photoUrl:string) {
+        this.title = title;
         this.description = description;
         this.photoUrl = photoUrl;
     }
