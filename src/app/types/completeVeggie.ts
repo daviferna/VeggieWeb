75bb92503bd6ab672c3a9f379168dfa97f529bc9
vvaquerizo <vvaquerizo@zennio.com>
2021-01-09 20:42:10 +0100

    Textos alternativos en imágenes, y adaptados types y assets \ WCAG 1.1.1

--- src/app/types/completeVeggie.ts
+++ src/app/types/completeVeggie.ts
@@ -10,9 +10,9 @@ export class CompleteVeggie extends Veggie{
     maxTemperature:number;
     plantInstructions:PlantInstruction[];
 
-    constructor(id:number, name:string, type:string, description:string, photoUrl:string, plantSeasson:string, harvestSeasson:string, minHumidity:number, maxHumidity:number, 
-        minTemperature:number, maxTemperature:number, plantInstructions:PlantInstruction[]){
-        super(id, name, type, description, photoUrl);
+    constructor(id:number, name:string, type:string, description:string, photoUrl:string, photoDescription:string, plantSeasson:string, harvestSeasson:string, minHumidity:number,
+        maxHumidity:number, minTemperature:number, maxTemperature:number, plantInstructions:PlantInstruction[]){
+        super(id, name, type, description, photoUrl, photoDescription);
         this.plantSeasson = plantSeasson;
         this.harvestSeasson = harvestSeasson;
         this.minHumidity = minHumidity;
