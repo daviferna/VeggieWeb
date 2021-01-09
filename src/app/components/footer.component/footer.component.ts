6148606eafe4bbe653ff6382014fe40860b48d1d
daniel.avila <daniel.avilaf@outlook.es>
2021-01-09 01:33:22 +0100

    Añadido footer con estilos y revisados estilos de home

--- src/app/components/footer.component/footer.component.ts
+++ src/app/components/footer.component/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'footer-component',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private translateService: TranslateService) { }
 
   ngOnInit(): void {
+  }
+
+  changeLanguage(language:string):void{
+    this.translateService.use(language);
+  }
+
+  isLanguageSelected(language:string):Boolean{
+    return this.translateService.currentLang == language;
   }
 
 }
